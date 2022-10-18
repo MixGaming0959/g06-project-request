701fb0c035652c5d66cc28950de56c40cc9a7417
MixGaming0959 <wasutew@gmail.com>
2022-10-18 19:00:43 +0700

    18/10/2565

--- backend/controller/room.go
+++ backend/controller/room.go
@@ -27,24 +27,30 @@ func CreateRoom(c *gin.Context) {
 func GetRoom(c *gin.Context) {
 	var room entity.Room
 	id := c.Param("id")
-	if err := entity.DB().Raw("SELECT * FROM rooms WHERE id = ?", id).Scan(&room).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": room})
-}
-func GetRoomBuilding(c *gin.Context) {
-	var room []entity.Room
-	building_id := c.Param("building_id")
-	// if err := entity.DB().Raw("SELECT * FROM rooms WHERE building_id = ?", id).Scan(&room).Error; err != nil {
+	// if err := entity.DB().Raw("SELECT * FROM rooms WHERE id = ?", id).Scan(&room).Error; err != nil {
 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	// 	return
 	// }
 
+	if tx := entity.DB().Where("id = ?", id).First(&room); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "room not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": room})
+}
+func GetRoomBuilding(c *gin.Context) {
+	var room []entity.Room
+	building_id := c.Param("id")
 	if err := entity.DB().Preload("Building").Raw("SELECT * FROM rooms WHERE building_id = ?", building_id).Scan(&room).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	// if err := entity.DB().Preload("Building").Raw("SELECT * FROM rooms WHERE building_id = ?", building_id).Find(&room).Error; err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
 
 	c.JSON(http.StatusOK, gin.H{"data": room})
 }
@@ -52,7 +58,7 @@ func GetRoomBuilding(c *gin.Context) {
 // GET /rooms
 func ListRooms(c *gin.Context) {
 	var rooms []entity.Room
-	if err := entity.DB().Raw("SELECT * FROM rooms").Scan(&rooms).Error; err != nil {
+	if err := entity.DB().Preload("Building").Raw("SELECT * FROM rooms").Find(&rooms).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
