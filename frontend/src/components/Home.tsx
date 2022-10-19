de5901734144edfb9836d506fc0a12a6bc123191
MixGaming0959 <wasutew@gmail.com>
2022-10-19 21:18:53 +0700

    display Role Level in Home UI

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,32 @@
-// import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { UsersInterface } from "../models/IUser";
+import { GetUser, GetRolebyUser } from "../services/HttpClientService";
 
 function Home() {
+  const [user, setUser] = useState<UsersInterface>({});
+  const getUser = async () => {
+    let res = await GetUser();
+    if (res) {
+      setUser(res);
+      console.log("Load User Complete");
+    }
+    else{
+      console.log("Load User InComplete!!!!");
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div>หน้าแรก</div>
+  
+    <div> 
+      `User Role Level: {user.Role?.Name}`
+    </div>
+
+
+
   )
 }
 
