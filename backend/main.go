5f39d95f27353641f0856beb9bff2abc19f47026
MixGaming0959 <wasutew@gmail.com>
2022-10-24 22:00:41 +0700

    add Authorizes router

--- backend/main.go
+++ backend/main.go
@@ -21,92 +21,92 @@ func main() {
 		router.Use(middlewares.Authorizes())
 		{
 			// User Routes 1
-			r.GET("/users", controller.ListUsers)
-			r.GET("/user/:id", controller.GetUser)
-			r.POST("/users", controller.CreateUser)
-			r.PATCH("/users", controller.UpdateUser)
-			r.DELETE("/users/:id", controller.DeleteUser)
+			router.GET("/users", controller.ListUsers)
+			router.GET("/user/:id", controller.GetUser)
+			router.POST("/users", controller.CreateUser)
+			router.PATCH("/users", controller.UpdateUser)
+			router.DELETE("/users/:id", controller.DeleteUser)
 
 			// Request Routes 2
-			r.GET("/requests", controller.ListRequests)
-			r.GET("/request/:id", controller.GetRequest)
-			r.POST("/requests", controller.CreateRequest)
-			r.PATCH("/requests", controller.UpdateRequest)
-			r.DELETE("/requests/:id", controller.DeleteRequest)
+			router.GET("/requests", controller.ListRequests)
+			router.GET("/request/:id", controller.GetRequest)
+			router.POST("/requests", controller.CreateRequest)
+			router.PATCH("/requests", controller.UpdateRequest)
+			router.DELETE("/requests/:id", controller.DeleteRequest)
 
 			// Cart Routes 3
-			r.GET("/carts", controller.ListCarts)
-			r.GET("/cart/:id", controller.GetCart)
-			r.POST("/carts", controller.CreateCart)
-			r.PATCH("/carts", controller.UpdateCart)
-			r.DELETE("/carts/:id", controller.DeleteCart)
+			router.GET("/carts", controller.ListCarts)
+			router.GET("/cart/:id", controller.GetCart)
+			router.POST("/carts", controller.CreateCart)
+			router.PATCH("/carts", controller.UpdateCart)
+			router.DELETE("/carts/:id", controller.DeleteCart)
 
 			// Room has Device Routes 4
-			r.GET("/room_has_devices", controller.ListRoom_has_Devices)
-			r.GET("/room_has_device/:id", controller.GetRoom_has_Device)
-			r.GET("/room_has_device/room/:id", controller.GetRHD_Device)
-			r.POST("/room_has_devices", controller.CreateRoom_has_Device)
-			r.PATCH("/room_has_devices", controller.UpdateRoom_has_Device)
-			r.DELETE("/room_has_devices/:id", controller.DeleteRoom_has_Device)
+			router.GET("/room_has_devices", controller.ListRoom_has_Devices)
+			router.GET("/room_has_device/:id", controller.GetRoom_has_Device)
+			router.GET("/room_has_device/room/:id", controller.GetRHD_Device)
+			router.POST("/room_has_devices", controller.CreateRoom_has_Device)
+			router.PATCH("/room_has_devices", controller.UpdateRoom_has_Device)
+			router.DELETE("/room_has_devices/:id", controller.DeleteRoom_has_Device)
 
 			// Device Routes 5
-			r.GET("/devices", controller.ListDevices)
-			r.GET("/device/:id", controller.GetDevice)
-			r.POST("/devices", controller.CreateDevice)
-			r.PATCH("/devices", controller.UpdateDevice)
-			r.DELETE("/devices/:id", controller.DeleteDevice)
+			router.GET("/devices", controller.ListDevices)
+			router.GET("/device/:id", controller.GetDevice)
+			router.POST("/devices", controller.CreateDevice)
+			router.PATCH("/devices", controller.UpdateDevice)
+			router.DELETE("/devices/:id", controller.DeleteDevice)
 
 			// History Routes 6
-			r.GET("/histories", controller.ListHistorys)
-			r.GET("/history/:id", controller.GetHistory)
-			r.POST("/histories", controller.CreateHistory)
-			r.PATCH("/histories", controller.UpdateHistory)
-			r.DELETE("/histories/:id", controller.DeleteHistory)
+			router.GET("/histories", controller.ListHistorys)
+			router.GET("/history/:id", controller.GetHistory)
+			router.POST("/histories", controller.CreateHistory)
+			router.PATCH("/histories", controller.UpdateHistory)
+			router.DELETE("/histories/:id", controller.DeleteHistory)
 
 			// Building Routes 7
-			r.GET("/buildings", controller.ListBuildings)
-			r.GET("/building/:id", controller.GetBuilding)
+			router.GET("/buildings", controller.ListBuildings)
+			router.GET("/building/:id", controller.GetBuilding)
 
 			// Room Routes 8
-			r.GET("/rooms", controller.ListRooms)
-			r.GET("/room/:id", controller.GetRoom)
-			r.GET("/rooms/building/:id", controller.GetRoomBuilding)
+			router.GET("/rooms", controller.ListRooms)
+			router.GET("/room/:id", controller.GetRoom)
+			router.GET("/rooms/building/:id", controller.GetRoomBuilding)
 
 			// Role Routes 9
-			r.GET("/roles", controller.ListRoles)
-			r.GET("/role/:id", controller.GetRole)
+			router.GET("/roles", controller.ListRoles)
+			router.GET("/role/:id", controller.GetRole)
 
 			// JobType Routes 10
-			r.GET("/jobtypes", controller.ListJobTypes)
-			r.GET("/jobtype/:id", controller.GetJobType)
+			router.GET("/jobtypes", controller.ListJobTypes)
+			router.GET("/jobtype/:id", controller.GetJobType)
 
 			// DMG Level Routes 11
-			r.GET("/dmglevels", controller.ListDMGLevels)
-			r.GET("/dmglevel/:id", controller.GetDMGLevel)
+			router.GET("/dmglevels", controller.ListDMGLevels)
+			router.GET("/dmglevel/:id", controller.GetDMGLevel)
 
 			// gender Routes 12
-			r.GET("/genders", controller.ListGenders)
-			r.GET("/gender/:id", controller.GetGender)
+			router.GET("/genders", controller.ListGenders)
+			router.GET("/gender/:id", controller.GetGender)
 
 			// estimate Routes 13
-			r.GET("/estimates", controller.ListEstimates)
-			r.GET("/estimate/:id", controller.GetEstimate)
+			router.GET("/estimates", controller.ListEstimates)
+			router.GET("/estimate/:id", controller.GetEstimate)
 
 			// Educational_background Routes 14
-			r.GET("/educational_backgrounds", controller.ListEducational_backgrounds)
-			r.GET("/educational_background/:id", controller.GetEducational_background)
+			router.GET("/educational_backgrounds", controller.ListEducational_backgrounds)
+			router.GET("/educational_background/:id", controller.GetEducational_background)
 
 			// brand Routes 15
-			r.GET("/brands", controller.ListBrands)
-			r.GET("/brand/:id", controller.GetBrand)
+			router.GET("/brands", controller.ListBrands)
+			router.GET("/brand/:id", controller.GetBrand)
 
 			// distributor Routes 16
-			r.GET("/distributors", controller.ListDistributors)
-			r.GET("/distributor/:id", controller.GetDistributor)
+			router.GET("/distributors", controller.ListDistributors)
+			router.GET("/distributor/:id", controller.GetDistributor)
 
 			// type Routes 17
-			r.GET("/types", controller.ListTypes)
-			r.GET("/type/:id", controller.GetType)
+			router.GET("/types", controller.ListTypes)
+			router.GET("/type/:id", controller.GetType)
 		}
 
 	}
